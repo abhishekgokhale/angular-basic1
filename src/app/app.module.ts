e2cf318479fa0a66eb8ca05fb739ea9e6223cf30
abhishekgokhale <abhishekgokhale1599@gmail.com>
2020-12-31 14:32:19 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, ContactComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
